a64220128418d6283703a448b368b4ff677f2e93
Aditya Chauhan <adityachauhan0369@gmail.com>
2026-02-17 13:41:42 +0530

    Project Initialised

---
+++ Avello/AvelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvelloApp.swift
+//  Avello
+//
+//  Created by Aditya Chauhan on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AvelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Avello
+//
+//  Created by Aditya Chauhan on 17/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
